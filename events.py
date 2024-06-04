f947c228b099618bfe9e222e76802889ba110a39
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-06-04 09:51:57 -0400

    Create events.py

---
+++ events.py
@@ -0,0 +1,9 @@
+class Events:
+    def __init__(self):
+        self.historical_events = self.load_historical_events()
+
+    def load_historical_events(self):
+        return []
+
+    def trigger_event(self):
+        pass
